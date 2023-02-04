ab94a19348e3d87d6f4169e217ee6e6191fa9854
TishyaaChaudhry <94584800+TishyaaChaudhry@users.noreply.github.com>
2023-02-04 17:05:46 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+app = Flask('movie_recommender')
+from models import get_model
+
+@app.route('/')
+def show_movie_recommender_form():
+    return render_template('movie_form.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = request.form
+    if request.method == 'POST':
+        model = get_model()
+        movie_title = request.form['title']
+        recommended_movie = model.predict(movie_title)
+        return render_template('resultsform.html', title = movie_title,   predicted_movies=recommended_movie)
